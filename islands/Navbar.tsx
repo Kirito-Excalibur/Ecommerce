a49c3de20e9298f6afc7bf50cda1a749c3d7bded
Nikhil <nikhilsimontoppo@gmail.com>
2022-09-28 12:15:17 +0530

    hope

--- islands/Navbar.tsx
+++ islands/Navbar.tsx
@@ -9,10 +9,7 @@ function Navbar() {
 
   return (
     <div style={""}>
-      <div
-        class="flex justify-between h-[86px] w-[360px]   mx-auto sm:w-[1770px]"
-      
-      >
+      <div class="flex justify-between h-[86px] w-[360px]   mx-auto sm:w-[1770px]">
         <span class=" mt-6 ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +29,11 @@ function Navbar() {
         </span>
 
         <ul
-          class={sidebar? "fixed flex flex-col left-0 bg-pink-500 pt-10 transition-all ease-in rounded-tr-lg h-full": " fixed flex flex-col  -left-48 bg-pink-500 pt-10 transition-all ease-in rounded-tr-lg h-full"}
+          class={
+            sidebar
+              ? "fixed z-30 flex flex-col left-0 bg-pink-500 pt-10 transition-all ease-in rounded-tr-lg h-full"
+              : " fixed z-30 flex flex-col  -left-48 bg-pink-500 pt-10 transition-all ease-in rounded-tr-lg h-full"
+          }
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -54,29 +55,37 @@ function Navbar() {
             {SidebarData.map((item, index) => {
               return (
                 <a href={item.path}>
-                   <li key={index} class={item.cname}>
-                  <span class="  text-center p-4  w-full block transition ease-in-out  hover:bg-gray-500 hover:duration-500">
-                    {item.title}
-                  </span>
-                </li>
+                  <li key={index} class={item.cname}>
+                    <span class="  text-center p-4  w-full block transition ease-in-out  hover:bg-gray-500 hover:duration-500">
+                      {item.title}
+                    </span>
+                  </li>
                 </a>
-               
               );
             })}
           </li>
         </ul>
 
-
-
-<ul class="flex gap-60  items-center ">
-  <li class="hidden hover:border-b-4 hover:border-black sm:block"> <a href=""> Women</a></li>
-  <li class="hidden hover:border-b-4 hover:border-black sm:block"> <a href="">Men</a> </li>
-  <li class="hidden hover:border-b-4 hover:border-black sm:block"> <a href="">New</a> </li>
-  <li class="hidden hover:border-b-4 hover:border-black sm:block"> <a href=""> Special Collections</a></li>
-</ul>
+        <ul class="flex gap-60  items-center ">
+          <li class="hidden hover:border-b-4 hover:border-black sm:block">
+            {" "}
+            <a href=""> Women</a>
+          </li>
+          <li class="hidden hover:border-b-4 hover:border-black sm:block">
+            {" "}
+            <a href="">Men</a>{" "}
+          </li>
+          <li class="hidden hover:border-b-4 hover:border-black sm:block">
+            {" "}
+            <a href="">New</a>{" "}
+          </li>
+          <li class="hidden hover:border-b-4 hover:border-black sm:block">
+            {" "}
+            <a href=""> Special Collections</a>
+          </li>
+        </ul>
 
         <span class="flex gap-3 mt-6">
-          
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -105,22 +114,22 @@ function Navbar() {
               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
             />
           </svg>
-          <a href="/cart"><svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-10 h-10"
-            
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-            />
-          </svg></a>
-          
+          <a href="/cart">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-10 h-10"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </a>
         </span>
       </div>
     </div>
--- routes/payment.tsx
+++ routes/payment.tsx
@@ -5,31 +5,35 @@ import Navbar from "../islands/Navbar.tsx";
 function payment() {
   return (
     <div
-      class="flex relative mx-auto flex-col border items-center"
-      style="width:360px"
+      class="flex relative mx-auto flex-col w-[360px] justify-center sm:w-[1770px] items-center"
+   
     >
       <Navbar />
       <span class=" w-full">
-        <p class="text-sm  text-gray-500">Home/Cart/Checkout/Payment</p>
+        <p class="text-sm text-gray-500">Home/Cart/Checkout/Payment</p>
         <h1 class="text-3xl font-bold">Order Payment</h1>
         <p class="text-gray-400 mb-4">
           All transactions are secure and <br /> ecrypted
         </p>
       </span>
-      <img
-        src="/images/card-front.svg"
-        class="-translate-x-6 z-10"
-        style={"width:266px"}
-        alt=""
-      />
+      <span class="sm:flex sm:items-center sm:justify-center sm:gap-[300px]">  
+        <span class="flex items-center justify-center flex-col mt-10">
       <img
         src="/images/card-back.svg"
-        class=" translate-x-6 z-0"
-        style={"width:266px;margin-top:-151px"}
+        class=" translate-x-6 w-[266px] sm:w-[506px] z-0  "
+   
         alt=""
       />
-      <div class="w-full">
-        <h1 class="font-bold mt-12">Credit Card</h1>
+      <img
+        src="/images/card-front.svg"
+        class="-translate-x-6 z-10 -mt-[241px]  sm:w-[506px]  w-[266px]"
+    
+        alt=""
+      />
+    
+      </span>
+      <div class="w-full  sm:w-[385px]">
+        <h1 class="font-bold sm:mt-12 mt-[130px]">Credit Card</h1>
         <input
           placeholder="Card number"
           style={"height:48px"}
@@ -64,15 +68,19 @@ function payment() {
           <h1> Amount Payable:</h1>
           <h1>169.98 BYN</h1>
         </span>
+        <a href="">
         <div
           style={"height:48px"}
           class="border w-full mt-7 text-white bg-black flex-row pl-4 pr-2 justify-center items-center rounded-3xl flex "
         >
-          <a href="">Pay</a>
+        Pay
         </div>
-
-        <Footer />
+        </a>
+       
       </div>
+  </span>
+    
+      <Footer />
     </div>
   );
 }
